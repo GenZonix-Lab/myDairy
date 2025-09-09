903974b2644f82a587beb957db7047861fe0d6a4
GenZonix-Lab <solutiongenzonix@gmail.com>
2025-09-09 12:54:25 +0530

    change auth client id

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -5,5 +5,5 @@ export const auth = referenceAuth({
   identityPoolId: 'ap-south-1:c8e4214b-cb3e-4419-9af3-00fbb3ccacd0',
   authRoleArn: 'arn:aws:iam::607221908091:role/amplify-d1z2cx9a18swvf-ma-amplifyAuthauthenticatedU-bUzdKhhBTS0d',
   unauthRoleArn: 'arn:aws:iam::607221908091:role/amplify-d1z2cx9a18swvf-ma-amplifyAuthunauthenticate-5VoedqOaNpLB',
-  userPoolClientId: '7o58kjmfpvd5ejbacpv5s8s704',
+  userPoolClientId: '6mthi5d6fhc6hv8jnou91p57cf',
 });
